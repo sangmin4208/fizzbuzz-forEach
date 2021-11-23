6be19933ebd03e81e3615dbdefce290bbe2743cd
Lee Sang-Min <sangmin4208@gmail.com>
2021-11-23 15:26:36 +0900

    Done - 3의 배수 fizz출력

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fizzbuzz-forEach
+
+[✅] 3의 배수 "fizz"  
+[] 5의 배수 "buzz"  
+[] 15의 배수 "fizzbuzz"  
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type myNumbers []int
+
+func (mn myNumbers) forEach(fn func(v int)) {
+	for _, v := range mn {
+		fn(v)
+	}
+}
+func main() {
+	var nums myNumbers = make([]int, 100)
+	for i := 1; i <= 100; i++ {
+		nums[i-1] = i
+	}
+
+	nums.forEach(func(v int) {
+
+		if v%3 == 0 {
+			fmt.Println("fizz")
+			return
+		}
+
+		fmt.Println(v)
+	})
+}
